ce84706d73a2b3069acffc2883379c81fc76ff11
“jsclibar” <jsclibar@gmail.com>
2025-04-22 22:55:41 -0300

    Teste

--- EXEMPLOS_INTRO_PYTHON/Entrada_Dados.py
+++ EXEMPLOS_INTRO_PYTHON/Entrada_Dados.py
@@ -7,4 +7,4 @@ print(f'A soma é: {soma}')
 # Por padrão, variáveis que recebem valores por meio da função input() são do tipo texto (string).
 # Por isso, ao somar n1 + n2 diretamente, o resultado será a junção dos dois textos, e não uma soma matemática.
 # Para que o valor seja tratado como número inteiro, é necessário converter com a função int() antes do input.
-# Para que o valor seja tratado como número real, é necessário converter com a função float() antes do input.
+# Para que o valor seja tratado como número real, é necessário converter com a função float() antes do input..
