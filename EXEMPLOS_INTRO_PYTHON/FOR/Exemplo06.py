f51ef3266649ab900b634fc900e499f86a729cfe
“jsclibar” <jsclibar@gmail.com>
2025-05-08 09:23:11 -0300

    Exemplos For e While

---
+++ EXEMPLOS_INTRO_PYTHON/FOR/Exemplo06.py
@@ -0,0 +1,7 @@
+inicio = int(input('Digite o número inicial: '))
+fim = int(input('Digite o número final: '))
+passo = int(input('Digite o passo: '))
+
+for c in range(inicio,fim+1, passo):
+    print(c)
+print('FIM')
