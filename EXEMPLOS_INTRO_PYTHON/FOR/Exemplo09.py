a83dad20548f96582ddef16558bc38a5bf7c4294
“jsclibar” <jsclibar@gmail.com>
2025-05-08 15:02:04 -0300

    Exemplos For e While

---
+++ EXEMPLOS_INTRO_PYTHON/FOR/Exemplo09.py
@@ -0,0 +1,8 @@
+numeros = [3, 7, 10, 15, 20]
+
+# Queremos encontrar o número 10 na lista
+for numero in numeros:
+    print("Verificando:", numero)
+    if numero == 10:
+        print("Número encontrado!")
+        break
