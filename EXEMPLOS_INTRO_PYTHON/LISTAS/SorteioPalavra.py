1c22fa4a1250c8dd4fbd716970d72fbae8030c94
“jsclibar” <jsclibar@gmail.com>
2025-05-23 08:06:30 -0300

    Exemplo Sortear Palavra

---
+++ EXEMPLOS_INTRO_PYTHON/LISTAS/SorteioPalavra.py
@@ -0,0 +1,7 @@
+import random
+
+lista_palavras = ['Campinas', 'Piracicaba', 'Sao Paulo', 'São Pedro', 'Rio das Pedras']
+palavra_sorteada = random.choice(lista_palavras)
+
+print(f'A palavra sorteada foi: {palavra_sorteada}')
+      
